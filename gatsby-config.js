11c850fa023737c55d506c38268ca11c43d15e5d
softtiny <yingxiancapatin@gmail.com>
2019-12-23 21:02:49 +0800

    test up

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,25 @@
 require('dotenv').config()
+var proxy = require("http-proxy-middleware")
 
 module.exports = {
   siteMetadata: {
     title: `Creative Portfolio`,
   },
+    developMiddleware: app => {
+        app.use(
+                  "/.netlify/functions/",
+            proxy({
+                        target: "http://localhost:9000",
+                pathRewrite: {
+                              "/.netlify/functions/": "",
+                            
+                },
+                      
+            })
+                
+        )
+          
+    },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
@@ -11,7 +27,7 @@ module.exports = {
     {
       resolve: `gatsby-source-datocms`,
       options: {
-        apiToken: process.env.DATO_API_TOKEN,
+        apiToken: '274d8906d11279d03343385b8753ac',
       },
     },
   ],
