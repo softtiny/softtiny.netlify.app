0f30105d3323d0e920654a361a6ffd3023b18fc7
mac <mac@mac.mac>
2020-06-11 16:38:56 +0800

    asb

--- src/pages/tree.js
+++ src/pages/tree.js
@@ -1,15 +1,13 @@
 import React from 'react'
+import Img from 'gatsby-image'
 import Layout from "../components/layout"
 import {  Link,graphql } from "gatsby"
-const Tree = ({data:{tree}}) =>{ 
+const Tree = ({data:{tree,datoCmsWork}}) =>{ 
     console.log(tree)
     return (
         <Layout>
             <div>
                 <article className="sheet">
-                    <div className="sheet__inner">
-                        <h1 className="sheet__title">TREE SVG LIST</h1>
-                    </div>
                     <div className="sheet__body">
                         <ul className="sidebar__menu">
                             <li>
@@ -22,6 +20,21 @@ const Tree = ({data:{tree}}) =>{
                             })}
                         </ul>
                     </div>
+                    <div className="sheet__inner">
+                        <h1 className="sheet__title">{datoCmsWork.title}</h1>
+                        <p className="sheet__lead">{datoCmsWork.excerpt}</p>
+                        
+                    </div>
+                    <div
+                        className="sheet__body"
+                          dangerouslySetInnerHTML={{
+                            __html: datoCmsWork.descriptionNode.childMarkdownRemark.html,
+                          }}
+                    />
+                    <div className="sheet__gallery">
+                      <Img fluid={datoCmsWork.coverImage.fluid} />
+                    </div>
+
                 </article>
                 
             </div>    
@@ -37,6 +50,21 @@ export const query = graphql`
             relativePath
           }
         } 
+    }
+    datoCmsWork(slug: {eq: "tree"}) {
+        title
+        excerpt
+        descriptionNode {
+            childMarkdownRemark {
+                html
+            }
+        }
+        coverImage {
+            url
+       	    fluid(maxWidth: 600,){
+              ...GatsbyDatoCmsSizes            
+            }
+        }
     }
   }
 `
