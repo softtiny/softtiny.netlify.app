0b7b14181c3594ec8f3f96f5be03aa6b0f4e245b
softtiny <yingxiancapatin@gmail.com>
2020-10-15 17:38:56 +0800

    "Thu Oct 15 17:38:56 2020"

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,6 +54,13 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
+        path: `${__dirname}/static/sitepkg`,// 文件路径
+        name: 'sitepkg' // 名称，可以用来过滤
+      }
+    }
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
         path: `${__dirname}/static/tree_svg`,// 文件路径
         name: 'tree_svg' // 名称，可以用来过滤
       }
